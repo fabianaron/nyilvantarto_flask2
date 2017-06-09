4eaa6cfbb706b4fa41be68d9012c6f53b8fb8685
fabianaron <soulsword97@gmail.com>
2017-06-09 13:30:29 +0200

    form validáció

--- static/js/app.js
+++ static/js/app.js
@@ -31,6 +31,29 @@ function switchMenu(clickedId) {
     return today;
   }
 
+  function removeAlert(munkanapId) {
+    $('#'+munkanapId+'.new_munkanap > .alert').remove();
+  }
+
+
+
+function putAlert(munkanapId, alertText) {
+  let existingAlert = $('#'+munkanapId+'.new_munkanap').find('.alerttext').text(); //tul biztositas?
+  if ( ( $ ('#'+munkanapId+'.new_munkanap > .alert').length === 0) ) { 
+    $('#'+munkanapId+'.new_munkanap').prepend(`
+      <div class="alert alert-warning alert-disminssable">
+        <a href="#" class="coles" data-dismiss="alert" aria-label="close">&times;</a>
+        <div class="alerttext">
+       `+alertText+` 
+        </div>
+      </div>
+      `);
+    } else {
+      //megvillogtatjuk a hibauzenetet
+    }
+  }
+
+
   function addMunkanap() {
     let lastid = parseInt($('.new_munkanap:last').attr('id'));
     console.log("lastid: "+lastid);
@@ -46,10 +69,10 @@ function switchMenu(clickedId) {
             <input id="`+(lastid+1)+`" type="text" placeholder="dátum" class=" datepicker datum_mezo form-control" value="`+mainap+`">
           </div>
           <div class="col-xs-6 col-sm-2">
-            <input id="`+(lastid+1)+`" type="number" min="0.5" max="12" step="0.5" class="munkaora form-control" placeholder="munkaóra">
+            <input id="`+(lastid+1)+`" type="number" min="0.5" max="8" step="0.5" class="munkaora form-control" placeholder="munkaóra">
           </div>
             <div class="col-xs-12 col-sm-6">
-              <textarea id="`+(lastid+1)+`" rows="3" class="form-control" placeholder="megjegyzés"></textarea>
+              <textarea id="`+(lastid+1)+`" rows="3" class="form-control comment" placeholder="megjegyzés"></textarea>
             </div>
           </div>
   		
@@ -63,17 +86,70 @@ function switchMenu(clickedId) {
   });
 
   }//addmunkanap
+  
   function removeMunkanap(munkanapToRemove) {
     $("#"+munkanapToRemove+".new_munkanap").remove();
     /*console.log("ezt kell torolni: "+munkanapToRemove);*/
   }
+
+  //globalis valtozo, hasznalat csak indokol esetben ajanlott!
   let new_munkanaps = [];
+  let collectHours = {};
   //[{id:1, datePiced:"2017.04.03",workedHour:5,comment:"mycomment",okToSend:true},{},{}...]
   
   function collectMunkanaps(){
     //TODO osszegyujteni a munkanapokat egy objecteket tartartalmazo
-    console.log("munkanapok osszegyujtese...");
+    new_munkanaps = [];
+    //az azonos napra beirt munkaorak szam nem haladhatja meg a 8-at
+    //collectHours ={"2017.06.09": 4, "2017.07.12":8 }
+    collectHours = {};
+    $('.new_munkanap').each(function(){
+      let munkanapId = $(this).attr('id');
+      let datePicked = $(this).find('.datepicker').val();
+      let workedHour = $(this).find('.munkaora').val();
+      workedHour = parseFloat(workedHour.replace(',','.').replace(' ',' '));
+      let comment_text = $(this).find('.comment').val();
+      let okToSend = false;
+/*      if ( collectHours[datePicked] ) { //ha letetik az adott datummal property az objectben
+        collectHours[datePicked] = collectHours[datePicked]; //akkor az erteke onmaga lesz, tehat nem bantjuk
+        } else { //kulonben
+          collectHours[datePicked] = 0; //letrehozzuk ezt a property-t es nullara allitjuk az erteket
+      }*/
+      
+      // a || operator ("vagy" jel) a bal oldalt fogja preferalni, ha igaz, ha az hamis, akkor a jobb oldalt fogja preferalni
+      collectHours[datePicked] = collectHours[datePicked] || 0;
+
+  /*collectHours[datePicked] = collectHours[datePicked] + workedHour; //minden esetben hozzaadjuk a property erteket
+      //a ledolgozott orat*/
+      collectHours[datePicked] += workedHour;
+
+      removeAlert(munkanapId);
+
+      if ( collectHours[datePicked] >= 0 && collectHours[datePicked] <=8 ) {
+        okToSend = true;
+      } else {
+        okToSend = false;
+        putAlert(munkanapId,"Ez a nap már elérte a max munakórát (8 óra)");
+      }
+      if ( workedHour === 0 ){
+        okToSend = false;
+        removeAlert(munkanapId);
+        putAlert(munkanapId, "A munkaóra nem lehet nulla");
+      }
+
+      new_munkanaps.push({
+        "id": munkanapId,
+        "datePicked": datePicked,
+        "workedHour": workedHour,
+        "comment": comment_text,
+        "okToSend": okToSend
+      });
+    });//each
+    console.log("a munkanapok: "+JSON.stringify(new_munkanaps));
+    console.log(collectHours);
   }
+
+  
 
 
   function sendForm() {
@@ -83,7 +159,7 @@ function switchMenu(clickedId) {
 
 $(document).on('blur','.munkaora',function(){
   //amit ide irunk kod,az akkor fut le ha a munkaora mezot elhagyja a user
-  munkaoraMezo = parseInt($(this).val());
+  munkaoraMezo = parseFloat($(this).val());
   /*console.log("a munkaora mezo erteke: "+munkaoraMezo);
   console.log("a munkaora mezo tipusa: "+typeof(munkaoraMezo));*/
   if ( Number.isFinite(munkaoraMezo) ){
